7312c812f5e0055776262be536326772995d3359
AhmedElmasry700 <ahmedsala.eg70@gmail.com>
2018-06-19 00:19:18 +0200

    update login

--- app/src/main/java/com/example/eslam/wheretogo/Log_in.java
+++ app/src/main/java/com/example/eslam/wheretogo/Log_in.java
@@ -4,16 +4,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Log_in extends AppCompatActivity {
     private TextView txt_signup;
+    private Button btn_signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
-        txt_signup=findViewById(R.id.txt_sign_up);
-        txt_signup.setOnClickListener(new View.OnClickListener() {
+        btn_signup = findViewById(R.id.btn_sign_up);
+        btn_signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent GoToSignUpPage = new Intent(Log_in.this, Home.class);
