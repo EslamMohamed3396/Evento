5c47ef369ade102618f353820ba30da5ea582628
AhmedElmasry700 <ahmedsala.eg70@gmail.com>
2018-06-19 01:34:28 +0200

    clean again

--- app/src/main/java/com/example/eslam/wheretogo/Log_in.java
+++ app/src/main/java/com/example/eslam/wheretogo/Log_in.java
@@ -5,10 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class Log_in extends AppCompatActivity {
-    private TextView txt_signup;
     private Button btn_signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
