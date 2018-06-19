f5231fd93f3f043ddedb9fde465c74cd4e83c216
AhmedElmasry700 <ahmedsala.eg70@gmail.com>
2018-06-19 04:23:42 +0200

    intent to home

--- app/src/main/java/com/example/eslam/wheretogo/Sign_Up.java
+++ app/src/main/java/com/example/eslam/wheretogo/Sign_Up.java
@@ -1,13 +1,26 @@
 package com.example.eslam.wheretogo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Sign_Up extends AppCompatActivity {
 
+    private Button btn_sign_two;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign__up);
+
+        btn_sign_two = findViewById(R.id.btn_sign_up_two);
+        btn_sign_two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent GoToSignUpPageTwo = new Intent(Sign_Up.this, Home.class);
+                startActivity(GoToSignUpPageTwo);
+            }
+        });
     }
 }
