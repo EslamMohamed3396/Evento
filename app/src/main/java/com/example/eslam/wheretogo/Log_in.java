2822760d568535758678db1b44c957aa55a69724
Eslam <em0109224@gmail.com>
2018-06-23 04:33:36 +0200

    Splash Screen and Add Validation

--- app/src/main/java/com/example/eslam/wheretogo/Log_in.java
+++ app/src/main/java/com/example/eslam/wheretogo/Log_in.java
@@ -5,24 +5,29 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Log_in extends AppCompatActivity {
 
-    private Button btn_signup, btn_log_in;
-
+    private Button btn_signup;
+    private Button btn_log_in;
+    private EditText mEmail;
+    private EditText mPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
-
+        mEmail = (EditText) findViewById(R.id.ed_email);
+        mPassword = (EditText) findViewById(R.id.ed_password);
         btn_log_in = (Button) findViewById(R.id.btn_log_in);
         btn_log_in.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent GoToHomePage = new Intent(Log_in.this, Bottom_Bar.class);
-                startActivity(GoToHomePage);
-                finish();
+                login();
             }
         });
 
@@ -34,5 +39,36 @@ public class Log_in extends AppCompatActivity {
                 startActivity(GoToSignUpPage);
             }
         });
+    }
+
+    private void login() {
+        if (validEmail(mEmail.getText().toString()) && validPassword(mPassword.getText().toString())) {
+            Intent GoToHomePage = new Intent(Log_in.this, Bottom_Bar.class);
+            startActivity(GoToHomePage);
+            //  finish();
+
+        } else {
+            if (!validEmail(mEmail.getText().toString())) {
+                mEmail.setError(getResources().getString(R.string.invalidEmail));
+                mEmail.requestFocus();
+            } else if (!validPassword(mPassword.getText().toString())) {
+                mPassword.setError(getResources().getString(R.string.invalidPassword));
+                mPassword.requestFocus();
+            }
+
+        }
+
+    }
+
+    private boolean validEmail(String mEmail) {
+        String emailPattern = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{3,3})$";
+        Pattern pattern = Pattern.compile(emailPattern);
+        Matcher matcher = pattern.matcher(mEmail);
+        return matcher.matches();
+    }
+
+    private boolean validPassword(String mPass) {
+
+        return !mPass.isEmpty() && mPass != null && mPass.length() > 6;
     }
 }
---
+++ app/src/main/java/com/example/eslam/wheretogo/Splash_Screen.java
@@ -0,0 +1,24 @@
+package com.example.eslam.wheretogo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class Splash_Screen extends AppCompatActivity {
+    private static final int TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash__screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash_Screen.this, Log_in.class);
+                startActivity(intent);
+                finish();
+            }
+        }, TIME_OUT);
+    }
+}
